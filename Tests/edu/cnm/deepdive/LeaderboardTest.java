6f360f91b07dbe64e30cbb5c98db445fbbce6981
Michael Lacey <laceymwes@gmail.com>
2018-06-16 16:34:02 -0600

    Reformat code. Remove unused imports.

--- Tests/edu/cnm/deepdive/LeaderboardTest.java
+++ Tests/edu/cnm/deepdive/LeaderboardTest.java
@@ -15,7 +15,7 @@ class LeaderboardTest {
 
   private final static int[] COMP_BOARD_TWO = {100, 90, 80, 80, 80, 20};
   private final static int[] SCORES_TWO = {10, 50, 80, 90};
-  private final static int[] COMP_TEST_TWO_RESULT = {7, 6, 3, 2}	;
+  private final static int[] COMP_TEST_TWO_RESULT = {7, 6, 3, 2};
   private final static int[] DENSE_TEST_TWO_RESULT = {5, 4, 3, 2};
 
 
--- src/edu/cnm/deepdive/Leaderboard.java
+++ src/edu/cnm/deepdive/Leaderboard.java
@@ -1,30 +1,20 @@
 package edu.cnm.deepdive;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.PrimitiveIterator;
 import java.util.Set;
 import java.util.TreeSet;
-import javax.lang.model.type.PrimitiveType;
 
 /**
- * The static methods of this class compute rankings of an input array of scores
- * against a leaderboard of scores.
+ * The static methods of this class compute rankings of an input array of scores against a
+ * leaderboard of scores.
  *
- * We want to determine Alice's rank as she progresses up the leaderboard. For
- * example, the four players on the leaderboard have high scores of 100, 90, 90,
- * and 80. Using dense ranking those players will have ranks 1, 2, 2, and 3
- * respectively. If Alice's scores are 70, 80 and 105, her rankings after each
- * game using dense ranking are 4, 3 and 1.
+ * We want to determine Alice's rank as she progresses up the leaderboard. For example, the four
+ * players on the leaderboard have high scores of 100, 90, 90, and 80. Using dense ranking those
+ * players will have ranks 1, 2, 2, and 3 respectively. If Alice's scores are 70, 80 and 105, her
+ * rankings after each game using dense ranking are 4, 3 and 1.
  *
- * You are given an array, <code>leaderboard</code>, of monotonically decreasing
- * leaderboard scores, and another array, <code>scores</code>, of Alice's scores for
- * the game. You must print j integers. The jth integer should indicate the current
- * rank of Alice after her jth game.
+ * You are given an array, <code>leaderboard</code>, of monotonically decreasing leaderboard scores,
+ * and another array, <code>scores</code>, of Alice's scores for the game. You must print j
+ * integers. The jth integer should indicate the current rank of Alice after her jth game.
  *
  * <h3>Sample Input</h3>
  * <pre><code>
@@ -49,23 +39,20 @@ import javax.lang.model.type.PrimitiveType;
  * 3
  * 1
  * </code></pre>
- *
- *
  */
 public class Leaderboard {
 
   /**
-   * Computes and returns an array of rankings, corresponding to the values in
-   * the <code>scores</code> array. These should use the common "competition"
-   * ranking scheme &ndash; for example, if 2 players are tied for
-   * 1<sup>st</sup> place, the player with the next lower score is assumed to
-   * in 3<sup>rd</sup> place. Another way of thinking about this scheme is that
-   * if a player is in <i>n</i><sup>th</sup> place, according to this scheme,
-   * then there are exactly (<i>n</i> - 1) players with a higher score.
+   * Computes and returns an array of rankings, corresponding to the values in the
+   * <code>scores</code> array. These should use the common "competition" ranking scheme &ndash; for
+   * example, if 2 players are tied for 1<sup>st</sup> place, the player with the next lower score
+   * is assumed to in 3<sup>rd</sup> place. Another way of thinking about this scheme is that if a
+   * player is in <i>n</i><sup>th</sup> place, according to this scheme, then there are exactly
+   * (<i>n</i> - 1) players with a higher score.
    *
    * @param leaderboard scores on the leaderboard, in non-ascending order.
-   * @param scores      scores to be ranked against leaderboard scores.
-   * @return            resulting ranks.
+   * @param scores scores to be ranked against leaderboard scores.
+   * @return resulting ranks.
    */
   public static int[] getCompetitionRanking(int[] leaderboard, int[] scores) {
     for (int i = 0; i < scores.length; ++i) {
@@ -83,18 +70,18 @@ public class Leaderboard {
   }
 
   /**
-   * Computes and returns an array of rankings, corresponding to the values in
-   * the <code>scores</code> array. These should use the dense ranking scheme
-   * &ndash; for example, if 2 players are tied for 1<sup>st</sup> place, the
-   * player with the next lower score is assumed to be in 2<sup>rd</sup> place.
-   * Another way of thinking about this scheme is that if a player is in
+   * Computes and returns an array of rankings, corresponding to the values in the
+   * <code>scores</code> array. These should use the dense ranking scheme &ndash; for example, if 2
+   * players are tied for 1<sup>st</sup> place, the player with the next lower score is assumed to
+   * be in 2<sup>rd</sup> place. Another way of thinking about this scheme is that if a player is
+   * in
    * <i>n</i><sup>th</sup> place, according to this scheme, then there are
-   * exactly (<i>n</i> - 1) higher score values, some of which may have been
-   * scored by more than one player.
+   * exactly (<i>n</i> - 1) higher score values, some of which may have been scored by more than one
+   * player.
    *
    * @param leaderboard scores on the leaderboard, in non-ascending order.
-   * @param scores      scores to be ranked against leaderboard scores.
-   * @return            resulting ranks.
+   * @param scores scores to be ranked against leaderboard scores.
+   * @return resulting ranks.
    */
 
   public static int[] getDenseRanking(int[] leaderboard, int[] scores) {
